64c19696b3b744863b2bf9bd0324ca40d0a1c7d8
kmsProjectMail <kmsProjectMail@gmail.com>
2020-12-08 16:17:15 +0900

    test commit

--- 20201208_Git_Project/src/com/min/edu/Git_Main.java
+++ 20201208_Git_Project/src/com/min/edu/Git_Main.java
@@ -5,5 +5,6 @@ public class Git_Main {
 	public static void main(String[] args) {
 		System.out.println("깃 이름이 main입니다.");
 		System.out.println("TEST");
+		System.out.println("추가");
 	}
 }
--- 20201208_Git_Project/src/com/min/edu/Git_Process.java
+++ 20201208_Git_Project/src/com/min/edu/Git_Process.java
@@ -3,4 +3,5 @@ package com.min.edu;
 public class Git_Process {
 
 	private int test;
+	private double t1;
 }
