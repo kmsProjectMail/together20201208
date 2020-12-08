5703a8ee8fc250086daae1e4c4799a64ffeec63b
kmsProjectMail <kmsProjectMail@gmail.com>
2020-12-08 16:14:19 +0900

    commit

--- 20201208_Git_Project/src/com/min/edu/Git_Main.java
+++ 20201208_Git_Project/src/com/min/edu/Git_Main.java
@@ -4,5 +4,6 @@ public class Git_Main {
 
 	public static void main(String[] args) {
 		System.out.println("깃 이름이 main입니다.");
+		System.out.println("TEST");
 	}
 }
--- 20201208_Git_Project/src/com/min/edu/Git_Process.java
+++ 20201208_Git_Project/src/com/min/edu/Git_Process.java
@@ -2,4 +2,5 @@ package com.min.edu;
 
 public class Git_Process {
 
+	private int test;
 }
